9c3240ff949e7e6cedd9ae3d044a8cd5d3365afe
Phil Hoffmann <philhoffmann1337@gmail.com>
2018-11-05 18:54:22 +0100

    Removed unnecessary font imports

--- preview/main.cpp
+++ preview/main.cpp
@@ -6,10 +6,6 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    QFontDatabase::addApplicationFont(":/FontAwesome/fa-solid-900.ttf");
-    QFontDatabase::addApplicationFont(":/FontAwesome/fa-regular-400.ttf");
-    QFontDatabase::addApplicationFont(":/FontAwesome/fa-brands-400.ttf");
-
     QQmlApplicationEngine engine;
     engine.addImportPath("qrc:///");
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
